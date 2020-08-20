f24025679aca2c74e8d9590b6555107bf45efe98
Aleksey <ancientt1k1@gmail.com>
2020-08-20 19:00:57 +0300

    react forms

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,13 +15,19 @@ export default class App extends Component {
         super(props);
         this.state = {
             data: [
-                {label: 'Going to learn react', important: true, id: nextId()},
-                {label: 'That is good', important: false, id: nextId()},
-                {label: 'I need a break...', important: false, id: nextId()}
-            ]
+                {label: 'Going to learn react', important: true, like: false, id: nextId()},
+                {label: 'That is good', important: false, like: false, id: nextId()},
+                {label: 'I need a break...', important: false, like: false, id: nextId()}
+            ],
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
     }
 
     deleteItem(id){
@@ -49,17 +55,82 @@ export default class App extends Component {
         })
     }
 
+    onToggle(id, property){
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const nextItem = {...old};
+            nextItem[property] = !old[property];
+
+            const newArr = [...data.slice(0, index), nextItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+        
+
+    onToggleImportant(id){
+        this.onToggle(id, 'important');
+    }
+
+    onToggleLiked(id){
+        this.onToggle(id, 'like');
+    }
+
+    searchPost(items, term){
+        if(term.length === 0){
+            return items;
+        }
+
+        return items.filter( (item) => {
+            return item.label.indexOf(term) > -1;
+        }); 
+    }
+
+    filterPost(items, filter){
+        if(filter === 'like'){
+            return items.filter(item => item.like);
+        } else {
+            return items;
+        }
+    }
+
+    onUpdateSearch(term){
+        this.setState({term});
+    }
+
+    onFilterSelect(filter){
+        this.setState({filter});
+    }
+
     render(){
+        const {data, term, filter} = this.state;
+
+        const liked = data.filter(item => item.like).length;
+        const allPosts = data.length;
+
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
         return (
             <div className="app">
-                <AppHeader/>
+                <AppHeader
+                    liked={liked}
+                    allPosts={allPosts}/>
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
+                    <PostStatusFilter
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <PostList 
-                    posts={this.state.data}
-                    onDelete={this.deleteItem}/>
+                    posts={visiblePosts}
+                    onDelete={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleLiked={this.onToggleLiked}/>
                 <PostAddFrom
                     onAdd={this.addItem}/>
             </div>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './search-panel.css';
 
-const SerachPanel = () => {
-    return (
-        <input
-            className="form-control search=input"
-            type="text"
-            placeholder="Поиск по записям"
-        />
-    )
-}
+export default class SerachPanel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
 
-export default SerachPanel;
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    }
+
+    onUpdateSearch(e){
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render(){
+        return (
+            <input
+                className="form-control search=input"
+                type="text"
+                placeholder="Поиск по записям"
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
+}
